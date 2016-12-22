4a03183dfc91ba9d24288908989bfb6ff7de90cb
mxl <1244514640@qq.com>
2016-12-22 09:40:26 +0800

    add: plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,24 +12,7 @@ module.exports = {
   output: require('./webpack/base/output.config.js'),
   module: require('./webpack/base/module.config.js'),
   resolve: require('./webpack/base/resolve.config.js'),
-  plugins: [
-    new CopyWebpackPlugin([
-      {from: 'app/index.html'},
-      {from: 'app/module00/index.html', to: 'module00'},
-      {from: 'app/module01/index.html', to: 'module01'},
-      {from: 'node_modules/jquery/dist/jquery.min.js', to: 'vendor'},
-    ]),
-    new ExtractTextPlugin('[name]/main.css'),
-    // new webpack.optimize.CommonsChunkPlugin('common/main.js',['main']),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //   },
-    //   output: {
-    //     comments: false,
-    //   },
-    // }),
-  ],
+  plugins: require('./webpack/base/plugins.config.js'),
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
     port: 4000,
